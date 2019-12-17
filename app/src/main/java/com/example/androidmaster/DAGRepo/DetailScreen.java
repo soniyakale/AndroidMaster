65fdb1e396f69082699df02c674968d593445e89
Soniya Kale <soniya.kale33@gmail.com>
2019-12-17 12:53:23 +0530

    First commi

---
+++ app/src/main/java/com/example/androidmaster/DAGRepo/DetailScreen.java
@@ -0,0 +1,16 @@
+package com.example.androidmaster.DAGRepo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidmaster.R;
+
+public class DetailScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_screen);
+    }
+}
